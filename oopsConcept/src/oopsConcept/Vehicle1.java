0b423e0ed119d1ad64e9565c729e0024c64db21e
AnuV2C18467 <AnuV2C18467@V2CP0081.vee2office.com>
2021-03-16 14:13:47 +0530

    first

--- oopsConcept/src/oopsConcept/Vehicle1.java
+++ oopsConcept/src/oopsConcept/Vehicle1.java
@@ -1,16 +1,16 @@
 package oopsConcept;
 
 public class Vehicle1{  
-	  //defining a method  
+
 	  void run(){System.out.println("Vehicle is running");}  
 	}  
-	//Creating a child class  
+	 
 	class Bike2 extends Vehicle1{  
-	  //defining the same method as in the parent class  
+	
 	  void run(){System.out.println("Bike is running safely");}  
 	  
 	  public static void main(String args[]){  
-	  Bike2 obj = new Bike2();//creating object  
-	  obj.run();//calling method  
+	  Bike2 obj = new Bike2();
+	  obj.run(); 
 	  }  
 	}
